75baff913045996db54f8db69a8e4a9a44d2f2f9
obidua <obiduatechnology@gmail.com>
2025-11-12 14:51:24 +0530

    Update build artifacts and service worker

--- apps/xmtp.chat/dev-dist/sw.js
+++ apps/xmtp.chat/dev-dist/sw.js
@@ -67,13 +67,10 @@ if (!self.define) {
     });
   };
 }
-define(['./workbox-f989abb0'], (function (workbox) { 'use strict';
+define(['./workbox-0d3d5bfe'], (function (workbox) { 'use strict';
 
-  self.addEventListener('message', event => {
-    if (event.data && event.data.type === 'SKIP_WAITING') {
-      self.skipWaiting();
-    }
-  });
+  self.skipWaiting();
+  workbox.clientsClaim();
 
   /**
    * The precacheAndRoute() method efficiently caches and responds to
@@ -85,7 +82,7 @@ define(['./workbox-f989abb0'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.en0pbu5c9vg"
+    "revision": "0.7j20dq92oeg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
